1aac25864084212c1425a8a9bdf2741831fd7333
Harrison <hmseaborn@gmail.com>
2020-05-21 15:48:01 -0700

    quiting time

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,5 @@
 const pgConnection =
-  process.env.DATABASE_URL ||
-  "postgres://hkozehmdlcedxd:729d8adb32f86e3203e0af8d4527e6a5b319fe15112293a1820eb5bacbc263ee@ec2-18-210-214-86.compute-1.amazonaws.com:5432/dbhn4nhshd4he1";
+  process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
 // make another change and deploy you know the drill but this time with a wipe of the node guys
 module.exports = {
   development: {
