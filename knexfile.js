5ca656fa4cadfe90ff20b7e8b3408bfeed5e468f
Harrison <hmseaborn@gmail.com>
2020-05-21 15:35:07 -0700

    wipe nodie guys push and pray some more

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const pgConnection =
   process.env.DATABASE_URL ||
   "postgres://hkozehmdlcedxd:729d8adb32f86e3203e0af8d4527e6a5b319fe15112293a1820eb5bacbc263ee@ec2-18-210-214-86.compute-1.amazonaws.com:5432/dbhn4nhshd4he1";
-// make another change and deploy
+// make another change and deploy you know the drill but this time with a wipe of the node guys
 module.exports = {
   development: {
     client: "sqlite3",
