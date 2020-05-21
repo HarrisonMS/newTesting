450f3f8492bcec9a90d109b5157848e584dd9853
Harrison <hmseaborn@gmail.com>
2020-05-21 15:27:30 -0700

    push and pray even harder

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 const pgConnection =
-  process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
+  process.env.DATABASE_URL ||
+  "postgres://hkozehmdlcedxd:729d8adb32f86e3203e0af8d4527e6a5b319fe15112293a1820eb5bacbc263ee@ec2-18-210-214-86.compute-1.amazonaws.com:5432/dbhn4nhshd4he1";
 // make another change and deploy
 module.exports = {
   development: {
